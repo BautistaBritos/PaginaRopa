4a3d559c37efd7b7f9808f4223423f984a9aa201
Bautista Britos <bautista.britos05@gmail.com>
2023-08-22 17:25:57 -0300

    Pre-entrega 2-JS

--- js/main.js
+++ js/main.js
@@ -1,48 +1,63 @@
-// Simulador Pre-entrega 1
+const teclados = [
+    {id: 1, objeto: "redragon kumara K552", precio: 5000},
+    {id: 2, objeto: "redragon yama", precio: 7000},
+    {id: 3, objeto: "redragon mitra", precio: 6000},
+    {id: 4, objeto: "hyperX alloy", precio: 8000},
+    {id: 5, objeto: "hyperX TKL", precio: 10000},
+];
 
-let usuario = parseInt(prompt("Ingrese su codigo bancario."))
+const mouses = [
+    {id: 6, objeto: "redragon M719", precio: 4000},
+    {id: 7, objeto: "redragon M998", precio: 7000},
+];
 
-if (isNaN(usuario)){
-    let userr = prompt("Ese no es un codigo valido. Vuelva a Ingresar el codigo.")
-    usuarioIngreso(userr)
-}else{
-    alert("Hola devuelta!")
-}
+const carro = [];
 
-function usuarioIngreso(user){
-    if(isNaN(user)){
-    for(let i = 0; i < 2; i++){
-        prompt("No ingreso un numero. Tendra que ingresarlo 2 veces mas para confirmarlo")
+const productos = [
+    {name: "teclados", productos: teclados},
+    {name: "mouses", productos: mouses},
+];
+
+let init = parseInt(prompt("Bienvenidos a VentasGamers. Para comprar precione 1, si desea retirarse precione cualquier otro caracter"))
+
+if(init == 1){
+    while (true) {
+        let inicio = parseInt(prompt("Bienvenidos a VentasGamers. Que desea llevar?" + " \n1- Teclados \n 2- Mouses \n 0- Salir de la compra"))
+
+        if(inicio === 0){
+            break;
         }
-        alert("Ingreso")
-    }else{
-        alert("Ingreso correctamente")
+
+        if (inicio < 0){
+            alert("El numero que ingresaste es menor a 0")
+        }else if(!isNaN(inicio)){
+            let categoria = productos[inicio - 1]
+
+            if (categoria) {
+                let lista= " "
+                categoria.productos.forEach((elemento, index) => {
+                    const recorrido = index + 1;
+                    lista += recorrido + ". " + elemento.objeto + " $" + elemento.precio + "\n"
+                })
+
+                let opciones = parseInt(prompt("Seleccione el numero del producto que desee agregar a su carro." + "\n" + lista))
+
+                if(opciones >= 1 && opciones <= categoria.productos.length){
+                    carro.push(categoria.productos[opciones - 1])
+                }else{
+                    alert("Usted selecciono una opcion invalida.")
+                }
+            }else{
+                alert("Categoria no valida.")
+            }
+        }else{
+            alert("Solo puede seleccionar entre el 1 y el 2.")
+        }
+
+    
     }
+}else{
+    alert("Gracias por su atencion.")
 }
 
-let bienvenido = parseInt(prompt("Bienvenido al Banco DineroAsegurado. Para retirar dinero precione 1, si desea finalizar precione 2."))
-
-switch (bienvenido) {
-    case 1:
-        let maquina = parseInt(prompt("Ingrese el monto a retirar. Maximo $50000"))
-        retiro(maquina)
-        break;
-    case 2:
-        alert("Finalizo la operacion.")
-        break;
-    default:
-        alert("Opcion no valida")
-        break;
-}
-
-function retiro(dinero){
-    if(dinero > 50000){
-        alert("Excedio el monto.")
-    }else if(dinero < 0){
-        alert("Numero no valido.")
-    }else if(isNaN(dinero)){
-        alert("No es un numero")
-    }else{
-        alert("Retiro el monto de" + " " + "$" + dinero + " " + "correctamente.")
-    }
-}
+alert("usted ha comprado" + carro) 
